af24cd1a0d343cb53ba9c7ad1f53aaad79b78539
BrookeGibbons <brooke.gibbons@uwa.edu.au>
2026-07-31 13:14:32 +0800

    cleaning species names

--- 01_Download and format data for app/RLS Scripts working/03_format_rls_m1_and_m2_data.R
+++ 01_Download and format data for app/RLS Scripts working/03_format_rls_m1_and_m2_data.R
@@ -113,7 +113,7 @@ species_in_multiple_classes <- m1_species %>%
   count() %>%
   filter(n > 1)
 
-m1_species_new <- dplyr::left_join(m1_species, CheckEM::aus_synonyms) %>%
+m1_clean <- dplyr::left_join(m1_species, CheckEM::aus_synonyms) %>%
   dplyr::mutate(genus = ifelse(!genus_correct%in%c(NA), genus_correct, genus)) %>%
   dplyr::mutate(species = ifelse(!is.na(species_correct), species_correct, species)) %>%
   dplyr::mutate(family = ifelse(!is.na(family_correct), family_correct, family)) %>%
@@ -131,16 +131,77 @@ m1_species_new <- dplyr::left_join(m1_species, CheckEM::aus_synonyms) %>%
   dplyr::mutate(genus = if_else(recorded_species_name %in% "Nesogobius spp.", "Unknown", genus)) %>%
   dplyr::mutate(species = if_else(species %in% "gigas", "spp", species)) %>%
   dplyr::mutate(genus_fam = if_else(genus %in% "Unknown", family, genus)) %>%
+  dplyr::mutate(species = if_else(genus %in% "Heteroclinus", "spp", species)) %>%
   dplyr::mutate(portal_name = paste(genus_fam, species)) %>%
   dplyr::rename(rls_recorded_name = recorded_species_name, 
                 rls_reporting_name = reporting_name) %>%
   dplyr::filter(!family %in% "Unknown") %>% # have removed unknowns at the family level
-  dplyr::select(-c(phylum, class, order, genus_fam))
-  # dplyr::filter(!class %in% "Teleostei") # removed species that had multiple classes for Cheilodactylus spectabilis  
+  dplyr::select(-c(phylum, class, order, genus_fam)) %>%
+  dplyr::mutate(scientific = paste(family, genus, species)) %>%
+  dplyr::filter(!scientific %in% "Monacanthidae Unknown spp")
+# dplyr::filter(!class %in% "Teleostei") # removed species that had multiple classes for Cheilodactylus spectabilis  
 
 # check species not in CheckEM life history list ----
-m1_species_new_not_observed <- m1_species_new %>%
+m1_species_new_not_observed <- m1_clean %>%
   dplyr::distinct(family, genus, species) %>%
   dplyr::anti_join(., CheckEM::australia_life_history, by = c("family", "genus", "species"))
 
+# Method 2 Fish----
+m2_species <- m2_fish %>%
+  dplyr::filter(!recorded_species_name %in% c("No species found")) %>%
+  tidyr::separate(species_name, into = c("genus", "species"), extra = "merge") %>%
+  mutate(species = str_remove_all(species, "\\.")) %>%
+  dplyr::mutate(genus = if_else(family == genus, "Unknown", genus)) %>%
+  tidyr::replace_na(list(family = "Unknown", genus = "Unknown")) 
 
+# species_in_multiple_classes <- m2_species %>%
+#   dplyr::distinct(phylum, class, order, family, genus, species) %>%
+#   group_by(family, genus, species) %>%
+#   count() %>%
+#   filter(n > 1) # none
+
+# Check for synonyms using CheckEM list
+# synonyms_in_m2 <- dplyr::left_join(m2_species, CheckEM::aus_synonyms) %>%
+#   dplyr::filter(!is.na(genus_correct)) %>%
+#   dplyr::mutate('old name' = paste(family, genus, species, sep = " ")) %>%
+#   dplyr::mutate('new name' = paste(family_correct, genus_correct, species_correct, sep = " ")) %>%
+#   dplyr::select('old name', 'new name') %>%
+#   dplyr::distinct()
+
+m2_species_new <- dplyr::left_join(m2_species, CheckEM::aus_synonyms) %>%
+  dplyr::mutate(genus = ifelse(!genus_correct%in%c(NA), genus_correct, genus)) %>%
+  dplyr::mutate(species = ifelse(!is.na(species_correct), species_correct, species)) %>%
+  dplyr::mutate(family = ifelse(!is.na(family_correct), family_correct, family)) %>%
+  dplyr::select(-c(family_correct, genus_correct, species_correct)) %>%
+  dplyr::mutate(scientific = paste(family, genus, species)) %>%
+  
+  # Sasha's synonyms
+  dplyr::mutate(genus = str_replace_all(genus, "Ascarosepion", "Sepia")) %>%
+  dplyr::mutate(species = if_else(genus %in% "Heteroclinus", "spp", species)) %>%
+  dplyr::mutate(species = if_else(species %in% "gracilis", "spp", species)) %>%
+  dplyr::mutate(species = if_else(species %in% "ningulus", "spp", species)) %>%
+  dplyr::mutate(species = if_else(species %in% "sp 4 [groovedcheek]", "spp", species)) %>%
+  dplyr::mutate(species = if_else(species %in% "Platycephalus", "spp", species)) %>%
+  dplyr::mutate(species = if_else(species %in% "gigas", "spp", species)) %>%
+  
+  dplyr::mutate(species = if_else(species %in% "pipefish", "spp", species)) %>%
+  dplyr::mutate(genus = if_else(genus %in% "Unidentified", "Unknown", genus)) %>%
+  
+  dplyr::mutate(family = if_else(genus %in% "Peronedys", "Ophiclinidae", family)) %>%
+  dplyr::mutate(family = if_else(genus %in% "Ophiclinus", "Ophiclinidae", family)) %>%
+  
+  dplyr::mutate(genus_fam = if_else(genus %in% "Unknown", family, genus)) %>%
+  dplyr::mutate(portal_name = paste(genus_fam, species)) %>%
+  dplyr::rename(rls_recorded_name = recorded_species_name, rls_reporting_name = reporting_name) %>%
+  dplyr::filter(!family %in% "Unknown") %>%
+  dplyr::select(-c(phylum, class, order, genus_fam)) %>%
+  dplyr::filter(!species %in% "portusjacksoni egg")
+
+m2_species_new_not_observed <- m2_species_new %>%
+  dplyr::distinct(family, genus, species) %>%
+  dplyr::anti_join(., CheckEM::australia_life_history, by = c("family", "genus", "species"))
+
+# Save cleaned data ----
+write_rds(m1_clean, "data/tidy/rls_m1_count_and_length.rds")
+write_rds(m2_fish_clean, "data/tidy/rls_m2_fish_count_and_length.rds")
+write_rds(m2_inverts_clean, "data/tidy/rls_m2_inverts_count_and_length.rds")
