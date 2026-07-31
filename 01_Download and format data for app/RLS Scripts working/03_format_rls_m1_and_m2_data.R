7f0da09d661067e96d72479e1937145db10be9d8
BrookeGibbons <brooke.gibbons@uwa.edu.au>
2026-07-31 11:47:16 +0800

    cleaning up species naming

--- 01_Download and format data for app/RLS Scripts working/03_format_rls_m1_and_m2_data.R
+++ 01_Download and format data for app/RLS Scripts working/03_format_rls_m1_and_m2_data.R
@@ -85,20 +85,62 @@ m2_inverts_no_species <- m2_inverts %>%
 surveys_not_present_in_m2_invert_data <- anti_join(sl_m2, m2_inverts) 
 write_csv(surveys_not_present_in_m2_invert_data, "surveys_not_present_in_m2_inverts_data.csv")
 
-test <- anti_join(m2_fish_no_species, m2_inverts_no_species)
+# test <- anti_join(m2_fish_no_species, m2_inverts_no_species) # they are exactly the same!
+# TODO something weird is happening here I think
+
+# TODO Have emailed Sophie to see if this is a mistake - need someway to tell if 2 blocks are always done for all methods
 
 # Tidy species names ----
 # Start with method 1 ----
 m1_species <- m1 %>%
-  # distinct(phylum, class, order, family, recorded_species_name, species_name, reporting_name) %>%
-  dplyr::filter(!recorded_species_name %in% c("No species found")) %>%
-  # dplyr::filter(!species_name == reporting_name) %>% # I think reporting name is the same as species name except for spps.
-  # dplyr::filter(!recorded_species_name == species_name) %>% # the only ones that are changed are synonym changes I think
+  dplyr::filter(!recorded_species_name %in% c("No species found")) %>% # remove zeros - use survey list to add in zeros
   tidyr::separate(species_name, into = c("genus", "species"), extra = "merge") %>%
-  mutate(species = str_remove_all(species, "\\.")) %>%
-  dplyr::mutate(genus = if_else(family == genus, "Unknown", genus)) %>%
-  tidyr::replace_na(list(family = "Unknown", genus = "Unknown")) 
+  mutate(species = str_remove_all(species, "\\.")) %>% # remove dots from species names (spp.)
+  dplyr::mutate(genus = if_else(family == genus, "Unknown", genus)) %>% # Make genus Unknown if it is a family name
+  tidyr::replace_na(list(family = "Unknown", genus = "Unknown"))# replace blanks
 
+# Check CheckEM's synonyms 
+# synonyms_in_m1 <- dplyr::left_join(m1_species, CheckEM::aus_synonyms) %>%
+#   dplyr::filter(!is.na(genus_correct)) %>%
+#   dplyr::mutate('old name' = paste(family, genus, species, sep = " ")) %>%
+#   dplyr::mutate('new name' = paste(family_correct, genus_correct, species_correct, sep = " ")) %>%
+#   dplyr::select('old name', 'new name') %>%
+#   dplyr::distinct()
 
+species_in_multiple_classes <- m1_species %>%
+  dplyr::distinct(phylum, class, order, family, genus, species) %>%
+  group_by(family, genus, species) %>%
+  count() %>%
+  filter(n > 1)
+
+m1_species_new <- dplyr::left_join(m1_species, CheckEM::aus_synonyms) %>%
+  dplyr::mutate(genus = ifelse(!genus_correct%in%c(NA), genus_correct, genus)) %>%
+  dplyr::mutate(species = ifelse(!is.na(species_correct), species_correct, species)) %>%
+  dplyr::mutate(family = ifelse(!is.na(family_correct), family_correct, family)) %>%
+  dplyr::select(-c(family_correct, genus_correct, species_correct)) %>% # replace synonyms
+  dplyr::mutate(scientific = paste(family, genus, species)) %>%
+  
+  # Sasha's name changes
+  dplyr::mutate(genus = str_replace_all(genus, "Ascarosepion", "Sepia")) %>%
+  dplyr::mutate(family = if_else(genus %in% "Neatypus", "Microcanthidae", family)) %>%
+  dplyr::mutate(family = if_else(genus %in% "Ophiclinus", "Ophiclinidae", family)) %>%
+  dplyr::mutate(genus = if_else(genus %in% "Pelates", "Helotes", genus)) %>%
+  dplyr::mutate(species = if_else(genus %in% "Pseudocaranx", "spp", species)) %>%
+  dplyr::mutate(species = if_else(genus %in% "Cochleoceps", "spp", species)) %>%
+  dplyr::mutate(genus = if_else(genus %in% "Cochleoceps", "Unknown", genus)) %>%
+  dplyr::mutate(genus = if_else(recorded_species_name %in% "Nesogobius spp.", "Unknown", genus)) %>%
+  dplyr::mutate(species = if_else(species %in% "gigas", "spp", species)) %>%
+  dplyr::mutate(genus_fam = if_else(genus %in% "Unknown", family, genus)) %>%
+  dplyr::mutate(portal_name = paste(genus_fam, species)) %>%
+  dplyr::rename(rls_recorded_name = recorded_species_name, 
+                rls_reporting_name = reporting_name) %>%
+  dplyr::filter(!family %in% "Unknown") %>% # have removed unknowns at the family level
+  dplyr::select(-c(phylum, class, order, genus_fam))
+  # dplyr::filter(!class %in% "Teleostei") # removed species that had multiple classes for Cheilodactylus spectabilis  
+
+# check species not in CheckEM life history list ----
+m1_species_new_not_observed <- m1_species_new %>%
+  dplyr::distinct(family, genus, species) %>%
+  dplyr::anti_join(., CheckEM::australia_life_history, by = c("family", "genus", "species"))
 
 
